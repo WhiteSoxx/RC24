8019b6ac6c326e0d2c4dc4e0f7b496ee555fcd84
WhiteSoxx <itsnevas@gmail.com>
2024-12-11 23:39:07 +0000

    player command parser

--- player/player.c
+++ player/player.c
@@ -6,21 +6,19 @@
 
 int main(int argc, char *argv[])
 {
+    int i;
     char *GSIP = "localhost";
     int GSPort = 58000+GN;
 
-    for(int i = 0; i < argc; i++)
+    for(i = 0; i < argc; i++)
     {
-        printf("argv[%d]: %s\n", i, argv[i]);
         if(!strcmp(argv[i], "-n"))
         {
-            printf("n found\n");
             GSIP = argv[i+1];
             i++;
         }
         else if(!strcmp(argv[i], "-p"))
         {
-            printf("p found\n");
             GSPort = atoi(argv[i+1]);
             i++;
         }
@@ -28,6 +26,85 @@ int main(int argc, char *argv[])
 
     printf("GSIP: %s\n", GSIP);
     printf("GSPort: %d\n", GSPort);
-    
+
+    int PLID = -1;
+    int max_playtime = -1;
+    char *key[4];
+
+    char buffer[1024];
+    while(fgets(buffer, sizeof(buffer), stdin) != NULL)
+    {
+        buffer[strcspn(buffer, "\n")] = 0;
+        char *token = strtok(buffer, " ");
+        char* command[1024];
+        command[0] = token;
+
+        i = 1;
+        token = strtok(NULL, " ");
+        while(token != NULL)
+        {
+            command[i] = token;
+            i++;
+            token = strtok(NULL, " ");
+        }
+
+        if(command[0] == NULL)
+        {
+            printf("command[0] is NULL\n");
+            continue;
+        }
+        else if(!strcmp(command[0], "start"))
+        {
+            PLID = atoi(command[1]);
+            max_playtime = atoi(command[2]);
+            printf("start!\n");
+            continue;
+        } //FIXME outros comandos só depois do jogo ter começado?
+        else if(!strcmp(command[0], "try"))
+        {
+            key[0] = command[1];
+            key[1] = command[2];
+            key[2] = command[3];
+            key[3] = command[4];
+
+            //FIXME cores só podem ser algumas específicas, verificar aqui? switch case
+
+            printf("try!\n");
+            continue;
+        }
+        else if(!strcmp(command[0], "show_trials") || !strcmp(command[0], "st"))
+        {
+            printf("show trials!\n");
+            continue;
+        }
+        else if(!strcmp(command[0], "scoreboard") || !strcmp(command[0], "sb"))
+        {
+            printf("scoreboard!\n");
+            continue;
+        }
+        else if(!strcmp(command[0], "quit"))
+        {
+            printf("quit!\n");
+            continue;
+        }
+        else if(!strcmp(command[0], "exit"))
+        {
+            printf("exit!\n");
+            break;
+        }
+        else if(!strcmp(command[0], "debug"))
+        {
+            PLID = atoi(command[1]);
+            max_playtime = atoi(command[2]);
+            key[0] = command[3];
+            key[1] = command[4];
+            key[2] = command[5];
+            key[3] = command[6];
+
+            printf("debug!\n");
+            continue;
+        }
+
+    }
     return 0;
 }
